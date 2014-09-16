c8b59f5c3b79219545a4978022cd353819bb5399
derek wu <derek.wu@ribose.com>
2014-09-16 11:19:02 +0200

    Ready to deploy Version 1.0

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,39 +1,44 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.released
-#    @movies = Movie.all
   end
 
   def show
     @movie = Movie.find(params[:id])
   end
-
+  
   def edit
     @movie = Movie.find(params[:id])
   end
-
+  
   def update
     @movie = Movie.find(params[:id])
-    @movie.update(movie_params)
-    redirect_to @movie
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie successfully updated!"
+    else
+      render :edit
+    end
   end
 
   def new
     @movie = Movie.new
   end
-
+  
   def create
     @movie = Movie.new(movie_params)
-    @movie.save
-    redirect_to @movie
+    if @movie.save
+      redirect_to @movie, notice: "Movie successfully created!"
+    else
+      render :new
+    end
   end
-
+  
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_url
+    redirect_to movies_url, alert: "Movie successfully deleted!"
   end
-
+  
 private
 
   def movie_params
